95c393cf3e1d4b4235af7f0ca5646912a069803d
Julia Viehberger <tschuliiia@Air-von-Julia.fritz.box>
2025-03-02 11:06:03 +0100

    updated CV Test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,7 +15,11 @@ INSERT INTO education (applicant_id, educational_institution, specification, edu
 VALUES (1, 'TU Graz', 'Software Entwicklung Wirtschaft', 'MASTER', '2015-09-01', '2018-12-01');
 
 INSERT INTO job_experience (applicant_id, company, position, start_date, end_date, tasks)
-VALUES (1, 'Parkside GmbH', 'Software Quality Manager', '2012-08-01', '2016-06-01',
+VALUES (1, 'XiTrust GmbH', 'Senior Java Developer & Technical Team Lead', '2023-06-01', '2025-04-30',
+        'Technical Leadership & technical PO of a renewal of a legacy system, Analyzing old system and defining architecture for new development(Tech stack: Java 17/21, Spring Boot 3, Ms SQL & H2, JUnit 4/5, Mockito, Keycloak), Keycloak Integration, Rollout of SonarQube for development department, Rollout of Conventional Commits & Unified Code Formatter for whole development department, Guidance in Scrum Processes');
+
+INSERT INTO job_experience (applicant_id, company, position, start_date, end_date, tasks)
+VALUES (1, 'Parkside GmbH', 'Java Developer', '2019-03-17', '2023-05-30',
         'Developing Rest APIs; NoSQL databases(EspressoDB, Firestore) and relational databases such as PostgresSQL, MySQL, H2; Testing with TestNG, Mockito and PowerMock, JUnit; Micro Services with Apache Kafka; Firebase with Python Backend; Build Tools: most projects with Gradle; Continuous Integration & Delivery with Jenkins and Spinnaker; Basic knowledge of Docker & Kubernetes; Experience in agile Software Development with Scrum and SAFE');
 INSERT INTO job_experience (applicant_id, company, position, start_date, end_date, tasks)
 VALUES (1, 'Samsung', 'Software Trainer for Kids', '2016-07-01', '2018-09-01',
@@ -26,6 +30,9 @@ VALUES (1, 'TU Graz', 'Software Trainer & Instructor for the Remote Mentoring Pr
 INSERT INTO job_experience (applicant_id, company, position, start_date, end_date, tasks)
 VALUES (1, 'Bits for kids', 'Software Trainer for students', '2018-09-01', '2019-03-01',
         'Programming workshops for students in the age of 7 to 17 with Pocket Code, PocketPaint, Scratch, Microbits and Unity basics');
+INSERT INTO job_experience (applicant_id, company, position, start_date, end_date, tasks)
+VALUES (1, 'Netconomy', 'Software Quality Manager', '2012-08-01', '2016-06-01',
+'Quality and defect management, Risk analysis, Conducting trainings and workshops for customers - Test case generation & Automated GUI testing, Coordination of Testing Agents, CMS Editing');
 
 INSERT INTO certificate (applicant_id, name, description)
 VALUES (1, 'ISTQB', 'Certified Tester - Foundation Level');
